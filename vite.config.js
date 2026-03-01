1197d093a5494881dd1a042f656fcbf716a3d232
majestymewtwo <majestymewtwo@gmail.com>
2026-03-01 20:23:29 +0530

    Added self signed certs

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import path from "node:path";
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler'],],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
     tailwindcss(),
   ],
-  server : {
+  server: {
+    https: {
+      key: fs.readFileSync("./certs/server.key"),
+      cert: fs.readFileSync("./certs/server.crt"),
+    },
     host: true,
-    port : 5001
+    port: 443,
+    allowedHosts: ["educodetrack.com"],
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
-})
+});
