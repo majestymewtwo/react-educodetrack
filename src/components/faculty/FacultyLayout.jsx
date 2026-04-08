d9acef017f55948693a9ee5a7d6b6aa905bd0a4d
majestymewtwo <majestymewtwo@gmail.com>
2026-04-08 23:12:58 +0530

    import fix

--- src/components/faculty/FacultyLayout.jsx
+++ src/components/faculty/FacultyLayout.jsx
@@ -1,12 +1,12 @@
 import ProtectedRoute from "../common/ProtectedRoute";
-import SideBar from "./SideBar";
+import SideBar from "./Sidebar";
 
 export default function FacultyLayout({ children }) {
   return (
     <section className="flex">
       <SideBar />
       <ProtectedRoute type="faculty">{children}</ProtectedRoute>
-      
+
     </section>
   );
 }
